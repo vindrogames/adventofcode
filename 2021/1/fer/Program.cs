7b58d889a566f1a38f42b1ae6601cbb660ebed65
Fernando Gimenez <fernando.gimenez_cacho@siemens.com>
2021-12-01 11:53:46 +0100

    1.1 solved

--- 2021/1/fer/Program.cs
+++ 2021/1/fer/Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+var numbers = new List<int>();
+Boolean DEBUG = false;
+
+// Load data and put it in a list of integers
+foreach (string line in System.IO.File.ReadLines(@"data.txt"))
+{  
+    numbers.Add(Int32.Parse(line));     
+}
+
+if (DEBUG)
+{
+    foreach (int fuel in numbers)
+    {
+        System.Console.WriteLine(fuel);
+    }
+}
+
+int increases = 0;
+int previous = 0;
+
+foreach (int depth in numbers)
+{
+    if (depth > previous)
+    {
+        increases++;
+    }
+    previous = depth;
+}
+
+System.Console.WriteLine("Part 1 solution:");
+System.Console.WriteLine(increases-1);
