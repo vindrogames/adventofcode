548aa941294e41d6d5ed53e0c4fa5e80c5a3b802
Fernando Gimenez <fernando.gimenez_cacho@siemens.com>
2021-12-03 17:12:04 +0100

    fer -> solved using the windows calculator to multiply the last part, pending to do it right

--- 2021/3/dotnet/Program.cs
+++ 2021/3/dotnet/Program.cs
@@ -76,3 +76,144 @@ foreach (string bit in epsilon_rate)
 
 System.Console.WriteLine("Solution to part one:");
 System.Console.WriteLine(gamma_rate_decimal*epsilon_rate_decimal);
+
+System.Console.WriteLine("-------------------------------------------------------");
+
+int pos=0;
+
+while (pos < 12 && datas.Count > 1)
+{   
+    
+    int numberOfCeros = 0;
+    int numberOfOnes = 0;
+    //System.Console.WriteLine(pos);
+    foreach (string data in datas)
+    {
+        
+        if (data[pos] == '0')
+        {
+            numberOfCeros++;
+        }
+        else if (data[pos] == '1')
+        {
+            numberOfOnes++;
+        }
+    }
+    
+    
+    List<string> temp = new List<string>();
+
+    if (numberOfOnes >= numberOfCeros)
+    {
+        foreach (string data in datas)
+        {
+            if (data[pos] == '0')
+            {
+                temp.Add(data);
+            }
+        }
+    }
+    else
+    {
+        foreach (string data in datas)
+        {
+            if (data[pos] == '1')
+            {
+                temp.Add(data);
+            }
+        }
+    }
+    foreach (string item in temp)
+    {
+        datas.Remove(item);        
+    }
+    pos++;
+    /*foreach (string data in datas)
+    {
+        System.Console.WriteLine(data); 
+    }*/
+    //System.Console.WriteLine(pos); 
+    //System.Console.WriteLine(datas.Count); 
+}
+
+
+
+foreach (string data in datas)
+{
+    System.Console.WriteLine(data);
+}
+
+var datas_dos = new List<string>();
+
+// Load data and put it in a list of integers
+foreach (string line in System.IO.File.ReadLines(@"data.txt"))
+{  
+    datas_dos.Add(line);
+}
+
+pos = 0;
+
+while (pos < 12 && datas_dos.Count > 1)
+{   
+    int numberOfCeros = 0;
+    int numberOfOnes = 0;
+    foreach (string data in datas_dos)
+    {
+        
+        if (data[pos] == '0')
+        {
+            numberOfCeros++;
+        }
+        else if (data[pos] == '1')
+        {
+            numberOfOnes++;
+        }
+    }
+    System.Console.WriteLine(numberOfCeros);
+    System.Console.WriteLine(numberOfOnes);
+    System.Console.WriteLine("--------------");
+    List<string> temp = new List<string>();
+
+    if (numberOfOnes >= numberOfCeros)
+    {
+        foreach (string data in datas_dos)
+        {
+            if (data[pos] == '1')
+            {
+                temp.Add(data);
+            }
+        }
+    }
+    else
+    {
+        foreach (string data in datas_dos)
+        {
+            if (data[pos] == '0')
+            {
+                temp.Add(data);
+            }
+        }
+    }
+    foreach (string item in temp)
+    {
+        datas_dos.Remove(item);
+    }
+    pos++;
+    /*foreach (string data in datas_dos)
+    {
+        System.Console.WriteLine(data); 
+    }
+    */
+    //System.Console.WriteLine(pos); 
+    //System.Console.WriteLine(datas_dos.Count); 
+    
+}
+
+System.Console.WriteLine("oxigen");
+
+foreach (string data in datas_dos)
+{
+    System.Console.WriteLine(data);
+}
+
+
