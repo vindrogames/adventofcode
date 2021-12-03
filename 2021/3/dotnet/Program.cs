a5065edef49a699c6ac1c8deff34622038d3327c
Fernando Gimenez <fernando.gimenez_cacho@siemens.com>
2021-12-03 10:00:23 +0100

    day 3 fer's start

---
+++ 2021/3/dotnet/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var datas = new List<string>();
+
+Boolean DEBUG = false;
+
+// Load data and put it in a list of integers
+foreach (string line in System.IO.File.ReadLines(@"data.txt"))
+{  
+    datas.Add(line);
+}
+
+if (DEBUG)
+{
+    foreach (string data in datas)
+    {
+        System.Console.WriteLine(data);
+    }
+}
