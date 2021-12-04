48b7bbcb3ef6d69fb3537e37e01b1b97d89e2707
zeneke <zeneke@gmail.com>
2021-12-04 11:12:08 +0100

    schafolding for day 4, fer's

---
+++ 2021/4/Program.cs
@@ -0,0 +1,19 @@
+﻿//Day 4
+
+var datas = new List<string>();
+var numbers_draw = new List<int>();
+
+Boolean DEBUG = false;
+
+// Load data and get the numbers to draw and the boards
+int line = 0;
+foreach (string line in System.IO.File.ReadLines(@"data.txt"))
+{
+    //the first line is the numbers to draw  
+    string[] numbers_draw_temp = line.Split(',');
+    foreach (char number in numbers_draw_temp)
+    {
+        
+    }
+    line++;
+}
