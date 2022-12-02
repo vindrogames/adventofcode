12a459eaf60baab0c7023cc04c7874c06c5c405b
zeneke <zeneke@gmail.com>
2022-12-02 19:04:24 +0100

    day 2 solved in python at first try

---
+++ 2022/2/python/aoc_2022_2.py
@@ -0,0 +1,40 @@
+test_data = ["A Y","B X","C Z"]
+
+file = open("input.txt","r")
+
+#file = test_data
+total_score = 0
+for line in file:    
+    j1 = line[0]
+    j2 =  line[2]
+    jugador1 = 0
+    jugador2 = 0
+    if (j1 == 'A'):
+        jugador1 = 1
+    if (j1 == 'B'):
+        jugador1 = 2
+    if (j1 == 'C'):
+        jugador1 = 3
+    if (j2 == 'X'):
+        jugador2 = 1
+    if (j2 == 'Y'):
+        jugador2 = 2
+    if (j2 == 'Z'):
+        jugador2 = 3
+    
+    if (jugador1 == jugador2):
+        total_score = total_score + 3 + jugador2
+    if (jugador1 == 1 and jugador2 ==2):
+        total_score = total_score + 6 + jugador2
+    if (jugador1 == 1 and jugador2 ==3):
+        total_score = total_score + 0 + jugador2
+    if (jugador1 == 2 and jugador2 ==1):
+        total_score = total_score + 0 + jugador2
+    if (jugador1 == 2 and jugador2 ==3):
+        total_score = total_score + 6 + jugador2
+    if (jugador1 == 3 and jugador2 ==1):
+        total_score = total_score + 6 + jugador2
+    if (jugador1 == 3 and jugador2 ==2):
+        total_score = total_score + 0 + jugador2    
+
+print(total_score)
