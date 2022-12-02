fdbd5d7f8d639af35b7ae57ccc5e45c547fcc8c0
zeneke <zeneke@gmail.com>
2022-12-02 20:59:49 +0100

    second part solved for day 1 and 2

--- 2022/1/python/aoc_2022_1.py
+++ 2022/1/python/aoc_2022_1.py
@@ -10,3 +10,27 @@ for line in file:
         food = int(line.strip('\n'))
         suma = suma + food
 print(maxima_suma)
+
+######## PART 2
+
+file = open("input.txt","r")
+suma = 0
+top1 = 0
+top2 = 0
+top3 = 0
+for line in file:    
+    if line.strip('\n') == "":        
+        if suma > top1:
+            top3 = top2
+            top2 = top1
+            top1 = suma
+        elif suma > top2:
+            top3 = top2
+            top2 = suma
+        elif suma > top3:
+            top3 = suma
+        suma = 0
+    else:        
+        food = int(line.strip('\n'))
+        suma = suma + food
+print(top1+top2+top3)
