1db09d53076d0cb9b898d221199f17b2958bdbb8
fernando.gimenez_cacho <fernando.gimenez_cacho@siemens.com>
2024-12-03 17:40:18 +0100

    day 3 part 1 finished using dotnet

--- 2024/3/dotnet/src/Program.cs
+++ 2024/3/dotnet/src/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+const bool TEST = false;
+string dataFile = "";
+
+if (TEST)
+{
+    dataFile = "test.txt";
+}
+else
+{
+    dataFile = "data.txt";
+}
+
+try
+{
+    List<int> group1 = [];
+    
+    using StreamReader reader = new(dataFile);
+    
+    // changed from string -> string? to indicate that the variable could be nullable (it was a warning)
+    string? line; 
+    int sumaFinal = 0;   
+    while ((line = reader.ReadLine()) != null)
+    {
+        sumaFinal += Extractor(line);
+    }
+    Console.WriteLine(sumaFinal);
+}
+catch (IOException e)
+{
+    Console.WriteLine("Cannot read file:");
+    Console.WriteLine(e.Message);
+}
+
+int Extractor(string line)
+{
+    // mul(2,4)
+    string pattern = @"mul\((\d+),(\d+)\)";
+    MatchCollection collection = Regex.Matches(line,pattern);
+    int sumaTotal = 0;
+    foreach (Match match in collection)
+    {
+        //Console.WriteLine(match.Groups[1].Value +' '+ match.Groups[2].Value);
+        
+        _ = int.TryParse(match.Groups[1].Value, out int val1);
+        _ = int.TryParse(match.Groups[2].Value, out int val2);
+        
+        sumaTotal = sumaTotal + (val1 * val2);
+    }
+    return sumaTotal;
+}
+
+class Extractor
+{
+
+}
