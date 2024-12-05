892f8176800c66a4894935b0e2999196184acb9a
fernando.gimenez_cacho <fernando.gimenez_cacho@siemens.com>
2024-12-05 17:43:19 +0100

    day3part2 added test files

--- 2024/3/dotnet/src/Program.cs
+++ 2024/3/dotnet/src/Program.cs
@@ -2,14 +2,17 @@
 
 const bool TEST = false;
 string dataFile = "";
+string dataFilePart2 = "";
 
 if (TEST)
 {
     dataFile = "test.txt";
+    dataFilePart2 = "test_part2.txt";
 }
 else
 {
     dataFile = "data.txt";
+    dataFilePart2 = "data_part2.txt";
 }
 
 try
@@ -51,7 +54,34 @@ int Extractor(string line)
     return sumaTotal;
 }
 
-class Extractor
-{
+// Part 2
 
+
+
+try
+{
+        
+    using StreamReader readerPart2 = new(dataFilePart2);
+    
+    // changed from string -> string? to indicate that the variable could be nullable (it was a warning)
+    string? line; 
+    int sumaFinal = 0;   
+    while ((line = readerPart2.ReadLine()) != null)
+    {
+        string pattern = @"^.+don't\(\)|do\(\)";
+        MatchCollection collection = Regex.Matches(line,pattern);
+        
+        foreach (Match match in collection)
+        {
+            Console.WriteLine(match.Groups[1].Value);          
+            
+        }
+        //sumaFinal += Extractor(line);
+    }
+    //Console.WriteLine(sumaFinal);
+}
+catch (IOException e)
+{
+    Console.WriteLine("Cannot read file:");
+    Console.WriteLine(e.Message);
 }
